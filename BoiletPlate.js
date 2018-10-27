40faf502eb1c04277c7d772fcdb6fa319e4ff71b
Help <pczerokey@gmail.com>
2018-10-27 02:25:05 -0400

    braces clean up

--- BoiletPlate.js
+++ BoiletPlate.js
@@ -7,105 +7,117 @@ BUILT IN INTENTS:
 cancel,stop,help,fallback,..
 
 */
-
-
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
-
 const Alexa = require('ask-sdk');
 const http = require(`http`);
 
 //checks if getnewfact intent was given
 const GetNewFactHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'LaunchRequest'
-      || (request.type === 'IntentRequest'
-        && request.intent.name === 'GetNewFactIntent');
-  },
-  //Previous code that would run if getnewfact intent
-   // handle(handlerInput) {
-  //   const factArr = data;
-  //   const factIndex = Math.floor(Math.random() * factArr.length);
-  //   const randomFact = factArr[factIndex];
-  //   const speechOutput = GET_FACT_MESSAGE + randomFact;
-
-  //   return handlerInput.responseBuilder
-  //     .speak(speechOutput)
-  //     .withSimpleCard(SKILL_NAME, randomFact)
-  //     .getResponse();
-  // },
-  //if getnewfact intent do this
-  handle(handlerInput) {
-    let options = {
-      host: `numbersapi.com`,
-      port: 80,
-      path: `/random/trivia`,
-      method: `GET`,
-    };
-    async function buildAlexaResponse(options){
-      let responseFromAPI = await httpRequestPromise(options);
-      return handlerInput.responseBuilder
-      .speak(GET_FACT_MESSAGE+responseFromAPI)
-      .getResponse();
+    canHandle(handlerInput)
+    {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'LaunchRequest' ||
+            (request.type === 'IntentRequest' &&
+                request.intent.name === 'GetNewFactIntent');
     }
-    return buildAlexaResponse(options);
-  }
+    , //Previous code that would run if getnewfact intent
+    // handle(handlerInput) {
+    //   const factArr = data;
+    //   const factIndex = Math.floor(Math.random() * factArr.length);
+    //   const randomFact = factArr[factIndex];
+    //declaring what is going to be said
+    //   const speechOutput = GET_FACT_MESSAGE + randomFact; // + HELP_REPROMPT
+
+    //   return handlerInput.responseBuilder
+    //speak what you want said
+    //     .speak(speechOutput)
+    //     .withSimpleCard(SKILL_NAME, randomFact)
+//reprompt is basically lett you keep going  
+           .reprompt(HELP_REPROMPT);
+    //     .getResponse();
+    // },
+    //if getnewfact intent do this
+    handle(handlerInput)
+    {
+        let options = {
+            host: `numbersapi.com`
+            , port: 80
+            , path: `/random/trivia`
+            , method: `GET`
+        , };
+        async function buildAlexaResponse(options)
+        {
+            let responseFromAPI = await httpRequestPromise(options);
+            return handlerInput.responseBuilder
+                .speak(GET_FACT_MESSAGE + responseFromAPI)
+                .getResponse();
+        }
+        return buildAlexaResponse(options);
+    }
 };
 
 const HelpHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest'
-      && request.intent.name === 'AMAZON.HelpIntent';
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(HELP_MESSAGE)
-      .reprompt(HELP_REPROMPT)
-      .getResponse();
-  },
-};
+    canHandle(handlerInput)
+    {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+            request.intent.name === 'AMAZON.HelpIntent';
+    }
+    , handle(handlerInput)
+    {
+        return handlerInput.responseBuilder
+            .speak(HELP_MESSAGE)
+            .reprompt(HELP_REPROMPT)
+            .getResponse();
+    }
+, };
 
 const ExitHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest'
-      && (request.intent.name === 'AMAZON.CancelIntent'
-        || request.intent.name === 'AMAZON.StopIntent');
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(STOP_MESSAGE)
-      .getResponse();
-  },
-};
+    canHandle(handlerInput)
+    {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+            (request.intent.name === 'AMAZON.CancelIntent' ||
+                request.intent.name === 'AMAZON.StopIntent');
+    }
+    , handle(handlerInput)
+    {
+        return handlerInput.responseBuilder
+            .speak(STOP_MESSAGE)
+            .getResponse();
+    }
+, };
 
 const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    canHandle(handlerInput)
+    {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'SessionEndedRequest';
+    }
+    , handle(handlerInput)
+    {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
 
-    return handlerInput.responseBuilder.getResponse();
-  },
-};
+        return handlerInput.responseBuilder.getResponse();
+    }
+, };
 
 const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
+    canHandle()
+    {
+        return true;
+    }
+    , handle(handlerInput, error)
+    {
+        console.log(`Error handled: ${error.message}`);
 
-    return handlerInput.responseBuilder
-      .speak('Sorry, an error occurred.')
-      .reprompt('Sorry, an error occurred.')
-      .getResponse();
-  },
-};
+        return handlerInput.responseBuilder
+            .speak('Sorry, an error occurred.')
+            .reprompt('Sorry, an error occurred.')
+            .getResponse();
+    }
+, };
 
 //ALEXA fact name to start fact skill
 const SKILL_NAME = 'Silly Number Facts';
@@ -133,37 +145,42 @@ const STOP_MESSAGE = 'Goodbye!';
 //   'The Moon is moving approximately 3.8 cm away from our planet every year.',
 // ];
 
-async function httpRequestPromise(options) {
-  //log the options in case we need to debug
-  console.log(`~~~~~~~~~~~~~~~${JSON.stringify(options)}~~~~~~~~~~~~~`);
-  // return new pending promise
-  return new Promise((resolve, reject) => {
-    const request = http.request(options, (response) => {
-      // reject the promise if the HTTP status isn't `Successful 2xx`
-      if (response.statusCode < 200 || response.statusCode > 299) {
-        reject(new Error('Failed to load page, status code: ' + response.statusCode));
-      }
-      // crete an array for holding data temporarily
-      const body = [];
-      // every time we get data, grab the chunk and push it to the array
-      response.on('data', (chunk) => body.push(chunk));
-      // when we are done, resolve the promise with all the chunks joined
-      response.on('end', () => resolve(body.join('')));
+async function httpRequestPromise(options)
+{
+    //log the options in case we need to debug
+    console.log(`~~~~~~~~~~~~~~~${JSON.stringify(options)}~~~~~~~~~~~~~`);
+    // return new pending promise
+    return new Promise((resolve, reject) =>
+    {
+        const request = http.request(options, (response) =>
+        {
+            // reject the promise if the HTTP status isn't `Successful 2xx`
+            if (response.statusCode < 200 || response.statusCode > 299)
+            {
+                reject(new Error('Failed to load page, status code: ' + response.statusCode));
+            }
+            // crete an array for holding data temporarily
+            const body = [];
+            // every time we get data, grab the chunk and push it to the array
+            response.on('data', (chunk) => body.push(chunk));
+            // when we are done, resolve the promise with all the chunks joined
+            response.on('end', () => resolve(body.join('')));
+        });
+        // reject the promise if there are connection errors of the request
+        request.on('error', (err) => reject(err));
+        request.end();
     });
-    // reject the promise if there are connection errors of the request
-    request.on('error', (err) => reject(err));
-    request.end();
-  });
 }
 
 const skillBuilder = Alexa.SkillBuilders.standard();
 
+//must appear in same order as this, and register here for new intent
 exports.handler = skillBuilder
-  .addRequestHandlers(
-    GetNewFactHandler,
-    HelpHandler,
-    ExitHandler,
-    SessionEndedRequestHandler
-  )
-  .addErrorHandlers(ErrorHandler)
-  .lambda();
+    .addRequestHandlers(
+        GetNewFactHandler
+        , HelpHandler
+        , ExitHandler
+        , SessionEndedRequestHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
